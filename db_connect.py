d5aeab7d67da0ea3e0fd93085709beed5ff210b0
Jhonathas <jhonathas.souza351@gmail.com>
2022-01-20 13:03:18 -0300

    Conexao com o db concluida

--- db_connect.py
+++ db_connect.py
@@ -1,27 +1,28 @@
 from ssl import cert_time_to_seconds
 import psycopg2
 import pandas as pd
-from sqlalchemy import create_engine
+
 
 class banco_dados:
     def ConexaoDatabase(self):
         #configuração para conexao do banco
-        self.engine = create_engine('postgresql://postgres:(adm8081)@192.168.0.13:5433/teste')
+        self.connection = psycopg2.connect(user = "postgres", password = "(adm8081)", host = "localhost", port = "5433")
         #Cursor para usar o SQL
-        self.cursor =  self.engine.cursor()
+        self.cursor = self.connection.cursor()
 
     def DesconectaDatabase(self):
-        self.engine.close()
+        self.connection.close()
 
     def MontaTabelas(self):
         self.ConexaoDatabase(); print("Conectando ao banco de dados")
         #Criacao da tabela
-        self.sql("""
-            CREATE TABLE IF NOT EXISTS clientes(
+        self.Sql_Insert = ("""
+            CREATE public clientes(
                 cod INTEGER PRIMARY KEY,
                 comando CHAR(40) NOT NULL,
                 chave_acesso INTEGER(20)
                 );
             """)
-        self.engine.commit()
+        self.cursor.execute(self.Sql_Insert)
+        self.connection.commit()
         self.DesconectaDatabase()
